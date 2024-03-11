f609681c0973d327a364d6576e8bcf13b8eb49df
lokeshkhabiya <lokeshkhabiya0011@gmail.com>
2024-03-11 23:28:19 +0530

    Minor billboard fixes

--- ecommerce-admin/app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
+++ ecommerce-admin/app/(dashboard)/[storeId]/(routes)/billboards/components/client.tsx
@@ -5,8 +5,14 @@ import { Heading } from "@/components/ui/heading"
 import { Separator } from "@/components/ui/separator"
 import { Plus } from "lucide-react"
 import { useParams, useRouter } from "next/navigation"
+import { Billboard } from "@prisma/client"
 
-export const BillboardClient = () => {
+interface BillboardClientProps {
+    data: Billboard[]
+}
+export const BillboardClient: React.FC<BillboardClientProps> = ({
+    data
+}) => {
     
     const router = useRouter();
     const params = useParams();
@@ -15,7 +21,7 @@ export const BillboardClient = () => {
         <>
             <div className="flex items-center justify-between">
                 <Heading
-                    title="Billboards (0)"
+                    title={`Billboards (${data.length})`}
                     description="Manage billboards of your store" />
                 <Button onClick={() => router.push(`/${params.storeId}/billboards/new`)}>
                     <Plus className="mr-2 h-4 w-4" />
